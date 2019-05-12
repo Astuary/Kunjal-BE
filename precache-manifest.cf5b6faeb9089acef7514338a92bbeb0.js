5d7d1095c99a63906ec76e88b8b3238387d7bbc9
Astuary <32832266+Astuary@users.noreply.github.com>
2019-05-12 09:40:18 +0530

    Updates

---
+++ precache-manifest.cf5b6faeb9089acef7514338a92bbeb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2855211625484d6b84a6d9fedfa74dbe",
+    "url": "/kunjalpanchal/index.html"
+  },
+  {
+    "revision": "fccdce81baf836f0f950",
+    "url": "/kunjalpanchal/static/css/main.054a64ed.chunk.css"
+  },
+  {
+    "revision": "b38278087eb09a93b7b6",
+    "url": "/kunjalpanchal/static/js/2.f18a303d.chunk.js"
+  },
+  {
+    "revision": "fccdce81baf836f0f950",
+    "url": "/kunjalpanchal/static/js/main.e2c101da.chunk.js"
+  },
+  {
+    "revision": "8563552503210e14ebaa",
+    "url": "/kunjalpanchal/static/js/runtime~main.d39a7987.js"
+  },
+  {
+    "revision": "71e7be2f81cbbffa3ebf3ea523ce9e4b",
+    "url": "/kunjalpanchal/static/media/Resume.71e7be2f.pdf"
+  },
+  {
+    "revision": "d8215444dbab249579b68765f4f018ba",
+    "url": "/kunjalpanchal/static/media/me.d8215444.png"
+  }
+]);
